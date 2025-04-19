37565adb6004e6180cccf073662658f995b41061
Fagner Cruz <fagner@sti.ufpb.br>
2025-04-19 19:56:32 -0300

    Initial commit

---
+++ src/desafio/dio/Main.java
@@ -0,0 +1,16 @@
+package desafio.dio;
+
+import desafio.dio.models.Board;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
+    private static Board board;
+    private final static int BOARD_LIMIT = 9;
+
+    public static void main(String[] args) {
+
+    }
+}
